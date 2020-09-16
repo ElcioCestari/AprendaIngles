83c7f210da33f4c6770d4f0872d4d9d3b927ef09
Elcio Cestari Taira <elciotaira@gmail.com>
2020-09-16 10:46:31 -0400

    Initial commit

---
+++ app/src/main/java/com/elcio/aprendaingles/MainActivity.java
@@ -0,0 +1,40 @@
+package com.elcio.aprendaingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    SmartTabLayout smartTabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);//esconde linha que fica abaixo da actionbar
+
+        viewPager = findViewById(R.id.viewpager);
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add(R.string.bichos, BichosFragment.class)
+                        .add(R.string.numeros, NumerosFragment.class)
+                        .add(R.string.vogais, VogaisFragment.class)
+                        .create()
+        );
+
+        viewPager.setAdapter(adapter);
+
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
